7f764671a1e7d9e09843af120d8732bc78bfbb69
Paula Vasconcelos Gueiros <pvgueiros@gmail.com>
2024-11-14 13:08:49 -0300

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Paula Vasconcelos Gueiros on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SetTests/SetTests.swift
@@ -0,0 +1,17 @@
+//
+//  SetTests.swift
+//  SetTests
+//
+//  Created by Paula Vasconcelos Gueiros on 14/11/24.
+//
+
+import Testing
+@testable import Set
+
+struct SetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
